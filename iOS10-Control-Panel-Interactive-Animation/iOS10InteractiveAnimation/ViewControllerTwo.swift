d9b743c4f83fd60a0b02d565ba169a97837fe595
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2017-10-14 15:10:24 -0700

    Upgraded projects to Swift 3

--- iOS10-Control-Panel-Interactive-Animation/iOS10InteractiveAnimation/ViewControllerTwo.swift
+++ iOS10-Control-Panel-Interactive-Animation/iOS10InteractiveAnimation/ViewControllerTwo.swift
@@ -32,7 +32,7 @@ class ViewControllerTwo: UIViewController {
         
         paneView.frame = view.bounds
         paneView.center.y = view.center.y * (3 - 0.5)
-        paneView.backgroundColor = UIColor.gray()
+        paneView.backgroundColor = .gray
         paneView.layer.cornerRadius = 5
         view.addSubview(paneView)
         
